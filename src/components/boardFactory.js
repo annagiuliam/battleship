de8f46cdb5aec129d8ff7311b7a2025a16f15066
annagiuliam <annagiulia.musso@gmail.com>
2021-01-29 18:15:31 +0100

    added ship placement logic

--- src/components/boardFactory.js
+++ src/components/boardFactory.js
@@ -13,16 +13,60 @@ const boardFactory = (size) => {
   return board;
 };
 
-//   function freeTiles() {}
-//   const shipsSizes = [2, 3];
-//   const ships = [];
-//   const freeTiles = [...tiles];
-//   function generateShips(sizes) {
-//     shipsSizes.forEach((size) => {
-//       const newShip = shipFactory(size);
-//     });
-//   }
+function random0to9() {
+  return Math.floor(Math.random() * (10 - 0)) + 0;
+}
 
-//return { tiles };
+function randomStart() {
+  const coords = [random0to9(), random0to9()];
+  return coords;
+}
+
+function randomOrientation() {
+  return random0to9() > 5 ? "vertical" : "horizontal";
+}
+function placeShip(a, b) {
+  return board[a].indexOf(board[a][b]) + 3;
+}
+
+function generateShipCoords() {
+  const startCoords = randomStart();
+  const orientation = randomOrientation();
+  const x = startCoords[0];
+  const y = startCoords[1];
+  const row = board[x];
+  let shipCoords;
+  if (orientation === "horizontal") {
+    shipCoords = generateHorCoords(x, y, row);
+  } else {
+    shipCoords = "to be defined";
+  }
+
+  return shipCoords;
+}
+
+function generateHorCoords(x, y, row) {
+  let endPos = null;
+  let endCoords = null;
+  let shipCoords = [];
+  if (row.indexOf(row[y]) + (ship.length - 1) < 10) {
+    endPos = row.indexOf(row[y]) + (ship.length - 1);
+    endCoords = [x, endPos];
+
+    for (let i = y; i <= endPos; i++) {
+      shipCoords.push([x, i]);
+    }
+  } else if (row.indexOf(row[y]) - (ship.length - 1) > 0) {
+    endPos = row.indexOf(row[y]) - (ship.length - 1);
+    endCoords = [x, endPos];
+
+    for (let i = y; i >= endPos; i--) {
+      shipCoords.push([x, i]);
+    }
+  }
+
+  return shipCoords;
+}
+console.log(generateShipCoords());
 
 module.exports = boardFactory;
