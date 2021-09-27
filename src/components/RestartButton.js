dd11efeaa5ceb917f3b160b6f37166b96c5ea8ab
annagiuliam <annagiulia.musso@gmail.com>
2021-09-27 17:17:24 +0200

    added restart icon

--- src/components/RestartButton.js
+++ src/components/RestartButton.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { BsArrowRepeat } from "react-icons/bs";
 
 const RestartButton = (props) => {
   return (
     <div className="button-section">
       <button id="restart-btn" onClick={props.onClick}>
-        Restart Game
+        <span id="restart-text">Restart Game</span>
+        <BsArrowRepeat id="restart-icon" />
       </button>
     </div>
   );
